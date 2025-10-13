67ac546792d1494772018aa3da4af6b21e8b53ca
HariomKhaladkar <khaladkarhariom1804@gmail.com>
2025-10-13 08:46:48 +0530

    v2.0.0: Create new definitive version for deployment

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the User Service! Version 1.0.1 - DEPLOYED!');
+  // Change this line
+res.send('Hello from the User Service! Version 2.0.0 is LIVE!');
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
