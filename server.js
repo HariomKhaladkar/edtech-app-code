0b07dd86b705499f5798ef9e86599b29a2430b71
HariomKhaladkar <khaladkarhariom1804@gmail.com>
2025-10-13 09:46:31 +0530

    VERSION 3.0: Create definitive build

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 
 app.get('/', (req, res) => {
   // Change this line
-res.send('Hello from the User Service! The pipeline is FULLY AUTOMATED!');
+res.send('Hello from Version 3.0! The FINAL deployment!');
 });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
