40aec2528c3855006080ab1ff03c211d16c29af2
HariomKhaladkar <khaladkarhariom1804@gmail.com>
2025-10-14 11:38:28 +0530

    feat(user-service): Add Postman test collection

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const message = process.env.GREETING_MESSAGE || "Hello from the default User Ser
 
 app.get('/', (req, res) => {
   // Use the dynamic message
-  res.send(message);
+  res.send('Hello from Version 7.0! This is the FINAL check!');
 });
 
 app.listen(PORT, () => {
