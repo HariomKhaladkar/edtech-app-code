c6cd2ecfbc7fd185b07084c7d315cda8d9def4f8
HariomKhaladkar <khaladkarhariom1804@gmail.com>
2025-10-15 14:16:29 +0530

    feat(course-service): Final trigger for automated deployment

--- course-service/server.js
+++ course-service/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the Course Service! The FINAL, SECURE, and TESTED pipeline!');
+  res.send('Hello from the Course Service! The FINAL, SECURE, and TESTED pipeline! yay');
 });
 
 app.listen(PORT, () => {
